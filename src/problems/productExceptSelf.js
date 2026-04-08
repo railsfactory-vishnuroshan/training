0f8d9965f47febcc5dd545add04fb61a254c0fe0
sedin-liveshm <livesh.m@sedintechnologies.com>
2026-04-08 20:06:38 +0530

    finished productExceptSelf

--- src/problems/productExceptSelf.js
+++ src/problems/productExceptSelf.js
@@ -12,7 +12,19 @@ var runners = require("..");
  * @return {number[]}
  */
 function productExceptSelf(nums) {
-  // Write your solution here
+  const n = nums.length;
+  const result = new Array(n).fill(1);
+  let prefixProduct = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] *= prefixProduct;
+    prefixProduct *= nums[i];
+  }
+  let suffixProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= suffixProduct;
+    suffixProduct *= nums[i];
+  }
+  return result;
 }
 
 if (require.main === module) {
