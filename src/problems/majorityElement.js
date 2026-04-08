cbd954089d5ecd17aad4b603f49ef19864d39b91
gautham-sedin <gautham.r@sedintechnologies.com>
2026-04-08 18:31:50 +0530

    Solved majorityElements.js

--- src/problems/majorityElement.js
+++ src/problems/majorityElement.js
@@ -11,7 +11,23 @@ var runners = require("..");
  * @return {number}
  */
 function majorityElement(nums) {
-  // Write your solution here
+  let major = nums[0];
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === major) {
+      count++;
+    } else {
+      count--;
+    }
+
+    if (count === 0) {
+      major = nums[i];
+      count = 1;
+    }
+  }
+
+  return major;
 }
 
 if (require.main === module) {
