44536a93a5cceec00d0bbd1a108b1a867125d7e8
Sedin-HarishViswanath <harish.viswanath@sedintechnologies.com>
2026-04-08 15:13:12 +0530

    Solved FirstUniqueChar problem

--- src/problems/firstUniqueChar.js
+++ src/problems/firstUniqueChar.js
@@ -12,7 +12,17 @@ var runners = require("..");
  * @return {number}
  */
 function firstUniqueChar(s) {
-  // Write your solution here
+  const map = {};
+
+  for (let char of s) {
+    map[char] = (map[char] || 0) + 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] === 1) return i;
+  }
+
+  return -1;
 }
 
 if (require.main === module) {
