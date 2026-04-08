a155e29a577ee5f7b60da782bd9bfbea8d7e6b7e
sedin-liveshm <livesh.m@sedintechnologies.com>
2026-04-08 20:12:23 +0530

    finished validAnagram

--- src/problems/validAnagram.js
+++ src/problems/validAnagram.js
@@ -12,7 +12,18 @@ var runners = require("..");
  * @return {boolean}
  */
 function validAnagram(s, t) {
-  // Write your solution here
+  if (s.length !== t.length) return false;
+  const charCount = {};
+  for (let char of s) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  for (let char of t) {
+    if (!charCount[char]) {
+      return false;
+    }
+    charCount[char]--;
+  }
+  return true;
 }
 
 if (require.main === module) {
