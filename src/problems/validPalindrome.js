5be0fdbfabd074f604ffe6391c10f6acc8b7930a
Abinashini31 <abinashini@sedintechnologies.com>
2026-04-08 21:55:49 +0530

    First-Commit

--- src/problems/validPalindrome.js
+++ src/problems/validPalindrome.js
@@ -1,22 +1,35 @@
 "use strict";
-
+ 
 var runners = require("..");
-
+ 
 /**
- * Determine if a string is a palindrome, considering only alphanumeric characters
- * and ignoring case.
- *
- * Example: validPalindrome("A man, a plan, a canal: Panama") => true
- *
- * @param {string} s
- * @return {boolean}
- */
+* Determine if a string is a palindrome, considering only alphanumeric characters
+* and ignoring case.
+*
+* Example: validPalindrome("A man, a plan, a canal: Panama") => true
+*
+* @param {string} s
+* @return {boolean}
+*/
 function validPalindrome(s) {
-  // Write your solution here
+     let data = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+     let i=0;
+     let j=data.length-1;
+     while(i<j){
+      if(data[i]!=data[j]){
+        return false;
+      }
+      i++;
+      j--;
+     }
+ 
+return true
+  
 }
-
+ 
 if (require.main === module) {
   runners.validPalindrome(validPalindrome);
 }
-
+ 
 module.exports = validPalindrome;
+ 
