810d8e0fff6c2752c2ecaeddd87cbb5cdf140eae
Sedin-HarishViswanath <harish.viswanath@sedintechnologies.com>
2026-04-08 17:15:28 +0530

    Solved LongestCommonPrefix problem

--- src/problems/longestCommonPrefix.js
+++ src/problems/longestCommonPrefix.js
@@ -12,7 +12,18 @@ var runners = require("..");
  * @return {string}
  */
 function longestCommonPrefix(strs) {
-  // Write your solution here
+  if (!strs.length) return "";
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+      if (!prefix) return "";
+    }
+  }
+
+  return prefix;
 }
 
 if (require.main === module) {
