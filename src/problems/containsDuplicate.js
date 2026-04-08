fd5beaf0ff60418537339af936dce1a03ba3e805
aravind-S-33 <aravind.s@sedintechnologies.com>
2026-04-08 17:34:18 +0530

    Solved 2 problems

--- src/problems/containsDuplicate.js
+++ src/problems/containsDuplicate.js
@@ -12,6 +12,17 @@ var runners = require("..");
  */
 function containsDuplicate(nums) {
   // Write your solution here
+  const unique = new Set();
+
+  for(let num of nums)
+  {
+    if(unique.has(num))
+    {
+      return true;
+    }
+    unique.add(num);
+  }
+  return false;
 }
 
 if (require.main === module) {
