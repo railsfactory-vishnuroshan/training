96f13046015a41b0a55cc9144a072a2a32dda3f4
techieGurl-ss <sakthiisiva123@gmail.com>
2026-04-08 21:55:21 +0530

    Solved twoSum.js

--- src/problems/twoSum.js
+++ src/problems/twoSum.js
@@ -13,7 +13,15 @@ var runners = require("..");
  * @return {number[]}
  */
 function twoSum(nums, target) {
-  // Write your solution here
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    }
+
+    map[nums[i]] = i;
+  }
 }
 
 if (require.main === module) {
