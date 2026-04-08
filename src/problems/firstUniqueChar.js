d87723b0e81d44775c71578314da171ccd1f09d1
Abinashini31 <abinashini@sedintechnologies.com>
2026-04-08 22:45:15 +0530

    FirstUnique

--- src/problems/firstUniqueChar.js
+++ src/problems/firstUniqueChar.js
@@ -1,22 +1,37 @@
 "use strict";
-
+ 
 var runners = require("..");
-
+ 
 /**
- * Find the index of the first non-repeating character in a string.
- * Return -1 if no such character exists.
- *
- * Example: firstUniqueChar("leetcode") => 0
- *
- * @param {string} s
- * @return {number}
- */
+* Find the index of the first non-repeating character in a string.
+* Return -1 if no such character exists.
+*
+* Example: firstUniqueChar("leetcode") => 0
+*
+* @param {string} s
+* @return {number}
+*/
 function firstUniqueChar(s) {
+let freq={};
+  
+  for(let ch of s){
+    freq[ch]=(freq[ch]||0)+1;
+      
+  }
+  for(let i=0;i<s.length;i++){
+    if(freq[s[i]]==1){
+      return i;
+    }
+  }
+  return -1;
+  
+ 
+ 
   // Write your solution here
 }
-
+ 
 if (require.main === module) {
   runners.firstUniqueChar(firstUniqueChar);
 }
-
+ 
 module.exports = firstUniqueChar;
