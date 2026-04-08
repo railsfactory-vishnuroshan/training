9f9801adae6dc4801454c79a9e4151017734af0c
Sedin-Rajashree <rajashree@sedintechnologies.com>
2026-04-08 21:41:05 +0530

    Fix typo in validPalindrome.js character comparison

--- src/problems/validPalindrome.js
+++ src/problems/validPalindrome.js
@@ -18,7 +18,7 @@ function validPalindrome(s) {
   let i = 0, j = str.length-1;
   while(i<j)
   {
-    if( str[i] >= 'a' && str[i] <= 'z' || str[i] >= '0' && str[i] <= ]9' )
+    if( str[i] >= 'a' && str[i] <= 'z' || str[i] >= '0' && str[i] <= '9' )
     {
       if( str[j] >= 'a' && str[j] <= 'z' || str[j] >= '0' && str[j] <= '9')
       {
