c603237a0f3efd43fe5fdfaa63b586d414321ea5
Abinashini31 <abinashini@sedintechnologies.com>
2026-04-08 22:52:05 +0530

    Missing number

--- src/problems/missingNumber.js
+++ src/problems/missingNumber.js
@@ -1,21 +1,28 @@
 "use strict";
-
+ 
 var runners = require("..");
-
+ 
 /**
- * Given an array containing n distinct numbers in range [0, n], find the missing one.
- *
- * Example: missingNumber([3,0,1]) => 2
- *
- * @param {number[]} nums
- * @return {number}
- */
+* Given an array containing n distinct numbers in range [0, n], find the missing one.
+*
+* Example: missingNumber([3,0,1]) => 2
+*
+* @param {number[]} nums
+* @return {number}
+*/
 function missingNumber(nums) {
+    let n=nums.length;
+    let total=(n*(n+1))/2;
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+    }
+    return total-sum;
   // Write your solution here
 }
-
+ 
 if (require.main === module) {
   runners.missingNumber(missingNumber);
 }
-
+ 
 module.exports = missingNumber;
