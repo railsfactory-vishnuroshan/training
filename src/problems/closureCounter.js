d1fe56b1bd6ca0827abf9953c87a7ed5ce2424d2
Vishnu <vishnu@sedintechnologies.com>
2026-04-08 11:24:29 +0530

    disabled AI features for this workspace

--- src/problems/closureCounter.js
+++ src/problems/closureCounter.js
@@ -6,7 +6,8 @@ var runners = require("..");
  * Return an object with increment(), decrement(), and reset() methods that
  * maintain state via closure. Cannot use global variables.
  *
- * Example: const counter = createCounter(5); counter.increment() => 6
+ * Example 1: const counter = createCounter(5); counter.increment() => 6
+ * Example 2: const counter = createCounter(0); counter.decrement() => -1
  *
  * @param {number} n  Initial value
  * @return {{ increment: Function, decrement: Function, reset: Function }}
