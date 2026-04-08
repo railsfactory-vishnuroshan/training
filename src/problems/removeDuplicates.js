d988e3699f5c0fe2ee42b35e4a9322954c4460b5
Ehthikash <ehthikash13@gmail.com>
2026-04-08 21:08:02 +0530

    solved all problems

--- src/problems/removeDuplicates.js
+++ src/problems/removeDuplicates.js
@@ -12,18 +12,17 @@ var runners = require("..");
  * @return {number}
  */
 function removeDuplicates(nums) {
-  if (nums.length == 0) return 0;
-
-  let k = 1;
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] != nums[i - 1]) {
-      nums[k] = nums[i];
-      k++;
+if(nums.length === 0 ){
+    return 0;
+  }
+  let i =0;
+  for(let j = 1 ; j < nums.length ; j++){
+    if( nums[j] !== nums[i]){
+      i++;
+      nums[i] = nums[j];
     }
   }
-
-  return k;
+  return i+1;
 
 }
 
