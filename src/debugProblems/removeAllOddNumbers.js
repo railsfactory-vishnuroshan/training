61ccbd04391633c11714bc9eaec0f73ea85b9707
aravind-S-33 <aravind.s@sedintechnologies.com>
2026-04-09 09:44:03 +0530

    Solved all debug problems

--- src/debugProblems/removeAllOddNumbers.js
+++ src/debugProblems/removeAllOddNumbers.js
@@ -7,7 +7,9 @@ const numbers = [1, 3, 5, 2, 4];
 for (let i = numbers.length - 1; i >= 0; i--) {
   if (numbers[i] % 2 !== 0) {
     numbers.splice(i, 1);
+    i--;
   }
+
 }
 
 console.log("Even numbers only:", numbers); // Expected output is [2, 4]
